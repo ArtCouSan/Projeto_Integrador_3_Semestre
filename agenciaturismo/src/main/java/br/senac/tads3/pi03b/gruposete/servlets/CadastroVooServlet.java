73c50d6db17f3d4a65552a6824f1911b12e3189c
Rafael Ferreira <rafaelfs96@outlook.com>
2017-05-16 19:26:15 -0300

    Revert ","
    This reverts commit 1e7fff92310b0f61cd235c1614ab3b3df32b0ac3.

--- agenciaturismo/src/main/java/br/senac/tads3/pi03b/gruposete/servlets/CadastroVooServlet.java
+++ agenciaturismo/src/main/java/br/senac/tads3/pi03b/gruposete/servlets/CadastroVooServlet.java
@@ -30,37 +30,41 @@ public class CadastroVooServlet extends HttpServlet {
         boolean erro = false;
 
         String origem = request.getParameter("origem");
-//        if (origem == null || origem.length() < 1) {
-        //          erro = true;
-        //        request.setAttribute("erroOrigem", true);
-        //  }
+        if (origem == null || origem.length() < 1) {
+            erro = true;
+            request.setAttribute("erroOrigem", true);
+        }
         String destino = request.getParameter("destino");
-//        if (destino == null || destino.length() < 1) {
-        //          erro = true;
-        //        request.setAttribute("erroDestino", true);
-        //  }
+        if (destino == null || destino.length() < 1) {
+            erro = true;
+            request.setAttribute("erroDestino", true);
+        }
         String data_ida = request.getParameter("data_ida");
-//        if (data_ida == null || !"  /  /    ".equals(data_ida)) {
-        //          erro = true;
-        //        request.setAttribute("erroData_ida", true);
-        //  }
+        if (data_ida == null || !"  /  /    ".equals(data_ida)) {
+            erro = true;
+            request.setAttribute("erroData_ida", true);
+        }
         String data_volta = request.getParameter("data_volta");
-//        if (data_volta == null || !"  /  /    ".equals(data_volta)) {
-        //          erro = true;
-        //        request.setAttribute("erroData_volta", true);
-        //  }
-
-        int quantidade_passagens = Integer.parseInt(request.getParameter("quantidade_passagens"));
-//        if (quantidade_passagens < 1) {
-        //          erro = true;
-        //        request.setAttribute("erroQuantidade_passagens", true);
-        //  }
+        if (data_volta == null || !"  /  /    ".equals(data_volta)) {
+            erro = true;
+            request.setAttribute("erroData_volta", true);
+        }
+        int quantidade_passagens;
+        try {
+            quantidade_passagens = Integer.parseInt(request.getParameter("quantidade_passagens"));
+            if (quantidade_passagens < 1) {
+                erro = true;
+                request.setAttribute("erroQuantidade_passagens", true);
+            }
+        } catch (NumberFormatException e) {
+            quantidade_passagens = 0;
+        }
 
         float preco = Float.parseFloat(request.getParameter("preco"));
-//        if (preco < 0) {
-        //          erro = true;
-        //        request.setAttribute("erroPreco", true);
-        //  }
+        if (preco < 0) {
+            erro = true;
+            request.setAttribute("erroPreco", true);
+        }
 
         if (!erro) {
             Voo voo = new Voo(data_ida, data_volta, destino, origem, quantidade_passagens, preco, true);
