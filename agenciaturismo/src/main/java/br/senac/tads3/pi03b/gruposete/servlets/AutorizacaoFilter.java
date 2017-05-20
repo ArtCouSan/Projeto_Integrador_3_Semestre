992b8f81bfc4bb41533391f0e978b96fac922624
Rafael Ferreira <rafaelfs96@outlook.com>
2017-05-20 13:58:55 -0300

    desativei login

--- agenciaturismo/src/main/java/br/senac/tads3/pi03b/gruposete/servlets/AutorizacaoFilter.java
+++ agenciaturismo/src/main/java/br/senac/tads3/pi03b/gruposete/servlets/AutorizacaoFilter.java
@@ -1,81 +1,81 @@
-package br.senac.tads3.pi03b.gruposete.servlets;
-
-import br.senac.tads3.pi03b.gruposete.models.Usuario;
-
-import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-
-@WebFilter(filterName = "AutorizacaoFilter",
-        
-        servletNames = {"AlteraClienteServlet", "AlteraFuncionarioServlet", "AlteraHotelServlet", "AlteraVooServlet",
-                        "BuscaClienteServlet", "BuscaFuncionarioServlet", "BuscaHotelServlet", "BuscaVooServlet",
-                        "CadastroClienteServlet", "CadastroFuncionarioServlet", "CadastroHotelServlet", "CadastroVooServlet",
-                        "EditaClienteServlet", "EditaFuncionarioServlet", "EditaHotelServlet", "EditaVooServlet",
-                        "ExcluiClienteServlet", "ExcluiFuncionarioServlet", "ExcluiHotelServlet", "ExcluiVooServlet",
-                        "ListaClienteServlet", "ListaFuncionarioServlet", "ListaHotelServlet", "ListaVooServlet"},
-        
-        urlPatterns = {"/protegido/*"}
-)
-public class AutorizacaoFilter implements Filter {
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        // 1) Verificar se usuário está autenticado
-        // 1a) Cast dos objetos request e response
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
-
-        // 2) Tenta obter a sessao do usuario
-        HttpSession sessao = httpRequest.getSession();
-        Usuario usuario
-                = (Usuario) sessao.getAttribute("usuario");
-
-        // Usuario nulo significa que não está logado
-        // Redireciona para tela de login
-        if (usuario == null) {
-            httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
-            return;
-        }
-
-        // 3) Usuario está logado, então verifica se tem permissão
-        // para acessar a página.
-        if (verificarAcesso(usuario, httpRequest, httpResponse)) {
-            // Acesso a página está liberado.
-            chain.doFilter(request, response);
-        } else {
-            // Usuário não tem permissão de acesso a página.
-            httpResponse.sendRedirect(httpRequest.getContextPath() + "/LoginNaoAutorizado");
-        }
-    }
-
-    private static boolean verificarAcesso(Usuario usuario,
-            HttpServletRequest request, HttpServletResponse response) {
-        String paginaAcessada = request.getRequestURI();
-        String pagina = paginaAcessada.replace(request.getContextPath(), "");
-
-        if (pagina.endsWith("entrada")
-                && usuario.temPapel("ADMIN")) {
-            return true;
-        } else if (pagina.endsWith("agenda")
-                && usuario.temPapel("BASICO")) {
-            return true;
-
-        }
-        return false;
-    }
-
-    @Override
-    public void destroy() {
-
-    }
-
-}
+//package br.senac.tads3.pi03b.gruposete.servlets;
+//
+//import br.senac.tads3.pi03b.gruposete.models.Usuario;
+//
+//import javax.servlet.*;
+//import javax.servlet.annotation.WebFilter;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
+//import java.io.IOException;
+//
+//@WebFilter(filterName = "AutorizacaoFilter",
+//        
+//        servletNames = {"AlteraClienteServlet", "AlteraFuncionarioServlet", "AlteraHotelServlet", "AlteraVooServlet",
+//                        "BuscaClienteServlet", "BuscaFuncionarioServlet", "BuscaHotelServlet", "BuscaVooServlet",
+//                        "CadastroClienteServlet", "CadastroFuncionarioServlet", "CadastroHotelServlet", "CadastroVooServlet",
+//                        "EditaClienteServlet", "EditaFuncionarioServlet", "EditaHotelServlet", "EditaVooServlet",
+//                        "ExcluiClienteServlet", "ExcluiFuncionarioServlet", "ExcluiHotelServlet", "ExcluiVooServlet",
+//                        "ListaClienteServlet", "ListaFuncionarioServlet", "ListaHotelServlet", "ListaVooServlet"},
+//        
+//        urlPatterns = {"/protegido/*"}
+//)
+//public class AutorizacaoFilter implements Filter {
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+//        // 1) Verificar se usuário está autenticado
+//        // 1a) Cast dos objetos request e response
+//        HttpServletRequest httpRequest = (HttpServletRequest) request;
+//        HttpServletResponse httpResponse = (HttpServletResponse) response;
+//
+//        // 2) Tenta obter a sessao do usuario
+//        HttpSession sessao = httpRequest.getSession();
+//        Usuario usuario
+//                = (Usuario) sessao.getAttribute("usuario");
+//
+//        // Usuario nulo significa que não está logado
+//        // Redireciona para tela de login
+//        if (usuario == null) {
+//            httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
+//            return;
+//        }
+//
+//        // 3) Usuario está logado, então verifica se tem permissão
+//        // para acessar a página.
+//        if (verificarAcesso(usuario, httpRequest, httpResponse)) {
+//            // Acesso a página está liberado.
+//            chain.doFilter(request, response);
+//        } else {
+//            // Usuário não tem permissão de acesso a página.
+//            httpResponse.sendRedirect(httpRequest.getContextPath() + "/LoginNaoAutorizado");
+//        }
+//    }
+//
+//    private static boolean verificarAcesso(Usuario usuario,
+//            HttpServletRequest request, HttpServletResponse response) {
+//        String paginaAcessada = request.getRequestURI();
+//        String pagina = paginaAcessada.replace(request.getContextPath(), "");
+//
+//        if (pagina.endsWith("entrada")
+//                && usuario.temPapel("ADMIN")) {
+//            return true;
+//        } else if (pagina.endsWith("agenda")
+//                && usuario.temPapel("BASICO")) {
+//            return true;
+//
+//        }
+//        return false;
+//    }
+//
+//    @Override
+//    public void destroy() {
+//
+//    }
+//
+//}
--- agenciaturismo/src/main/java/br/senac/tads3/pi03b/gruposete/servlets/LoginNaoAutorizadoServlet.java
+++ agenciaturismo/src/main/java/br/senac/tads3/pi03b/gruposete/servlets/LoginNaoAutorizadoServlet.java
@@ -1,43 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package br.senac.tads3.pi03b.gruposete.servlets;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-/**
- *
- * @author Rafael Ferreira
- */
-@WebServlet(name = "LoginNaoAutorizadoServlet", urlPatterns = {"/LoginNaoAutorizado"})
-public class LoginNaoAutorizadoServlet extends HttpServlet {
-
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        request.getRequestDispatcher("jsp/erroNaoAutorizado.jsp").forward(request, response);
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package br.senac.tads3.pi03b.gruposete.servlets;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+///**
+// *
+// * @author Rafael Ferreira
+// */
+//@WebServlet(name = "LoginNaoAutorizadoServlet", urlPatterns = {"/LoginNaoAutorizado"})
+//public class LoginNaoAutorizadoServlet extends HttpServlet {
+//
+//    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        request.getRequestDispatcher("jsp/erroNaoAutorizado.jsp").forward(request, response);
+//    }
+//
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        processRequest(request, response);
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        processRequest(request, response);
+//    }
+//
+//    @Override
+//    public String getServletInfo() {
+//        return "Short description";
+//    }
+//}
