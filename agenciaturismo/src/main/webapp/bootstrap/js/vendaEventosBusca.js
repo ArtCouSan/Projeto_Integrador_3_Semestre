0ab47ef839f591a482abfab5c42470ee5f4a0f04
ArtCouSan <artcousan@gmail.com>
2017-05-27 23:17:30 -0300

    Merge origin/master
    Conflicts:
    	agenciaturismo/src/main/webapp/jsp/Buscar/BuscaCliente.jsp
    	agenciaturismo/src/main/webapp/jsp/Buscar/BuscaFuncionario.jsp
    	agenciaturismo/src/main/webapp/jsp/Buscar/BuscaHotel.jsp
    	agenciaturismo/src/main/webapp/jsp/Buscar/BuscaVoo.jsp
    	agenciaturismo/src/main/webapp/jsp/Layout/cabecalho.jsp
    	agenciaturismo/src/main/webapp/jsp/Listar/ListaCliente.jsp
    	agenciaturismo/src/main/webapp/jsp/Listar/ListaFuncionario.jsp
    	agenciaturismo/src/main/webapp/jsp/Listar/ListaHotel.jsp
    	agenciaturismo/src/main/webapp/jsp/Listar/ListaVoo.jsp

--- agenciaturismo/src/main/webapp/bootstrap/js/vendaEventosBusca.js
+++ agenciaturismo/src/main/webapp/bootstrap/js/vendaEventosBusca.js
@@ -1,12 +1,31 @@
 function pesquisaHotel() {
     var busca = document.querySelector(".pesquisaHotel").value;
     if (busca.length === 0) {
-        document.querySelector(".pesquisaHotel").innerHTML = "";
+        var table = document.getElementById("table");
+        while (table.firstChild) {
+            table.removeChild(table.firstChild);
+        }
         return;
     } else {
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.addEventListener("load", listarHotel);
         xmlhttp.open("GET", "BuscarHotelVenda?busca=" + busca);
+        xmlhttp.send();
+    }
+}
+
+function pesquisaVoo() {
+    var busca = document.querySelector(".pesquisaVoo").value;
+    if (busca.length === 0) {
+        var table = document.getElementById("table");
+        while (table.firstChild) {
+            table.removeChild(table.firstChild);
+        }
+        return;
+    } else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.addEventListener("load", listarVoo);
+        xmlhttp.open("GET", "BuscarVooVenda?busca=" + busca);
         xmlhttp.send();
     }
 }
@@ -17,37 +36,101 @@ function listarHotel(evt) {
 
     var obj = JSON.parse(texto);
 
-    for (var item of obj) {
-
-        console.log(item);
-    }
-
-}
-
-function pesquisaVoo() {
-    var busca = document.querySelector(".pesquisaVoo").value;
-    if (busca.length === 0) {
-        document.querySelector(".pesquisaVoo").innerHTML = "";
-        return;
-    } else {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.addEventListener("load", listarVoo);
-        xmlhttp.open("GET", "BuscarVooVenda?busca=" + busca);
-        xmlhttp.send();
-    }
-}
-
-function listarVoo(evt) {
-
     var table = document.getElementById("table");
 
     while (table.firstChild) {
         table.removeChild(table.firstChild);
     }
 
+    var thead = document.createElement("thead");
+
+    var tr = document.createElement("tr");
+
+    var thN = document.createElement("th");
+    var nome = document.createTextNode("Nome");
+    thN.appendChild(nome);
+
+    var thDe = document.createElement("th");
+    var data_e = document.createTextNode("Data de entrada");
+    thDe.appendChild(data_e);
+
+    var thDs = document.createElement("th");
+    var data_s = document.createTextNode("Data de saida");
+    thDs.appendChild(data_s);
+
+    var thP = document.createElement("th");
+    var preco = document.createTextNode("Preco");
+    thP.appendChild(preco);
+
+    var thQq = document.createElement("th");
+    var qtd_q = document.createTextNode("Quantidade de quartos");
+    thQq.appendChild(qtd_q);
+
+    var thQh = document.createElement("th");
+    var qtd_h = document.createTextNode("Quantidade de hospedes");
+    thQh.appendChild(qtd_h);
+
+    tr.appendChild(thN);
+    tr.appendChild(thDe);
+    tr.appendChild(thDs);
+    tr.appendChild(thP);
+    tr.appendChild(thQq);
+    tr.appendChild(thQh);
+
+    thead.appendChild(tr);
+
+    table.appendChild(thead);
+
+    var tbody = document.createElement("tbody");
+
+    for (var item in obj) {
+
+        var tr2 = document.createElement("tr");
+        var tdN = document.createElement("td");
+        var nomeD = document.createTextNode(obj[item].nome_hotel);
+        tdN.appendChild(nomeD);
+        var tdDe = document.createElement("td");
+        var data_eD = document.createTextNode(obj[item].data_entrada);
+        tdDe.appendChild(data_eD);
+        var tdDs = document.createElement("td");
+        var data_sD = document.createTextNode(obj[item].data_saida);
+        tdDs.appendChild(data_sD);
+        var tdP = document.createElement("td");
+        var precoD = document.createTextNode(obj[item].preco);
+        tdP.appendChild(precoD);
+        var tdQq = document.createElement("td");
+        var qtd_qD = document.createTextNode(obj[item].quantidade_quartos);
+        tdQq.appendChild(qtd_qD);
+        var tdQh = document.createElement("td");
+        var qtd_hD = document.createTextNode(obj[item].quantidade_hospedes);
+        tdQh.appendChild(qtd_hD);
+
+        tr2.appendChild(tdN);
+        tr2.appendChild(tdDe);
+        tr2.appendChild(tdDs);
+        tr2.appendChild(tdP);
+        tr2.appendChild(tdQq);
+        tr2.appendChild(tdQh);
+
+        tbody.appendChild(tr2);
+
+    }
+
+    table.appendChild(tbody);
+
+}
+
+function listarVoo(evt) {
+
     var texto = (evt.target.responseText);
 
     var obj = JSON.parse(texto);
+
+    var table = document.getElementById("table");
+
+    while (table.firstChild) {
+        table.removeChild(table.firstChild);
+    }
 
     var thead = document.createElement("thead");
 
@@ -122,97 +205,6 @@ function listarVoo(evt) {
         tr2.appendChild(tdDv);
         tr2.appendChild(tdQp);
         tr2.appendChild(tdP);
-
-        tbody.appendChild(tr2);
-
-    }
-
-    table.appendChild(tbody);
-
-}
-
-function listarHotel(evt) {
-
-    var texto = (evt.target.responseText);
-
-    var obj = JSON.parse(texto);
-
-    var table = document.getElementById("table");
-
-    if (table.hasChildNodes()) {
-        table.removeChild(table.childNodes[0]);
-    }
-
-    var thead = document.createElement("thead");
-
-    var tr = document.createElement("tr");
-
-    var thN = document.createElement("th");
-    var nome = document.createTextNode("Nome");
-    thN.appendChild(nome);
-
-    var thDe = document.createElement("th");
-    var data_e = document.createTextNode("Data de entrada");
-    thDe.appendChild(data_e);
-
-    var thDs = document.createElement("th");
-    var data_s = document.createTextNode("Data de saida");
-    thDs.appendChild(data_s);
-
-    var thP = document.createElement("th");
-    var preco = document.createTextNode("Preco");
-    thP.appendChild(preco);
-
-    var thQq = document.createElement("th");
-    var qtd_q = document.createTextNode("Quantidade de quartos");
-    thQq.appendChild(qtd_q);
-
-    var thQh = document.createElement("th");
-    var qtd_h = document.createTextNode("Quantidade de hospedes");
-    thQh.appendChild(qtd_h);
-
-    tr.appendChild(thN);
-    tr.appendChild(thDe);
-    tr.appendChild(thDs);
-    tr.appendChild(thP);
-    tr.appendChild(thQq);
-    tr.appendChild(thQh);
-
-    thead.appendChild(tr);
-
-    table.appendChild(thead);
-
-    var tbody = document.createElement("tbody");
-
-    for (var item of obj) {
-
-        var tr2 = document.createElement("tr");
-        var tdN = document.createElement("td");
-        var nomeD = obj
-        console.log(item);
-        tdN.appendChild(nomeD);
-        var tdDe = document.createElement("td");
-        var data_eD;
-        tdDe.appendChild(data_eD);
-        var tdDs = document.createElement("td");
-        var data_sD;
-        tdDs.appendChild(data_sD);
-        var tdP = document.createElement("td");
-        var precoD;
-        tdP.appendChild(precoD);
-        var tdQq = document.createElement("td");
-        var qtd_qD;
-        tdQq.appendChild(qtd_qD);
-        var tdQh = document.createElement("td");
-        var qtd_hD;
-        tdQh.appendChild(qtd_hD);
-
-        tr2.appendChild(tdN);
-        tr2.appendChild(tdDe);
-        tr2.appendChild(tdDs);
-        tr2.appendChild(tdP);
-        tr2.appendChild(tdQq);
-        tr2.appendChild(tdQh);
 
         tbody.appendChild(tr2);
 
