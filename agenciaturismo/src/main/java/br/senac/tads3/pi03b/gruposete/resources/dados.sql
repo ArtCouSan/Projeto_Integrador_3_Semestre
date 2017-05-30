b4754f39df48b39cc7ebe5290d0a0998fd307b9e
Rafael Ferreira <rafaelfs96@outlook.com>
2017-05-30 19:33:53 -0300

    .

--- agenciaturismo/src/main/java/br/senac/tads3/pi03b/gruposete/resources/dados.sql
+++ agenciaturismo/src/main/java/br/senac/tads3/pi03b/gruposete/resources/dados.sql
@@ -5,7 +5,7 @@ INSERT INTO Voo (data_ida, data_volta, destino, origem, preco, ativo) VALUES ();
 INSERT INTO Hotel (nome_hotel, data_entrada, data_saida, quantidade_quartos, quantidade_hospedes, preco, ativo) VALUES ();
 
 --adiciona Funcionários pré definidos
-INSERT INTO Funcionario (cargo, departamento, filial, celular, telefone, email, numero, cep, rua, bairro, cidade, complemento, nome, cpf, sexo, ativo, data_nasc, login, senha, acesso) VALUES ('Vendedor', 'Vendas', 'SP', '(11)13213-4654', '(11)1323-4654', 'aohfa@aosfi.com', 654, '23132-312', 'rua tal de sp', 'jardim', 'SP', '-', 'Rafael', '012.123.123-12', 'M', true, '12/12/1212', 'rafael', 'rafael', 'MASTER');
+INSERT INTO Funcionario (cargo, departamento, filial, celular, telefone, email, numero, cep, rua, bairro, cidade, complemento, nome, cpf, sexo, ativo, data_nasc, login, senha, acesso) VALUES ('Vendedor', 'Vendas', 'SP', '(11)13213-4654', '(11)1323-4654', 'aohfa@aosfi.com', 654, '23132-312', 'rua tal de sp', 'jardim', 'SP', '-', 'Rafael', '012.123.123-12', 'M', true, '12/12/1212', 'admin', 'admin', 'MASTER');
 
 --adiciona Clientes pré definidos
 INSERT INTO Cliente (celular, telefone, email, numero, cep, rua, bairro, cidade, complemento, nome, cpf, sexo, ativo, data_nasc) VALUES ();
