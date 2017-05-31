5d88804f0ea4273d4efb97e185c171e68744b34a
Rafael Ferreira <rafaelfs96@outlook.com>
2017-05-30 23:34:05 -0300

    .

--- agenciaturismo/src/main/java/br/senac/tads3/pi03b/gruposete/resources/database.sql
+++ agenciaturismo/src/main/java/br/senac/tads3/pi03b/gruposete/resources/database.sql
@@ -91,3 +91,22 @@ quantidade INT(50) NOT NULL,
 preco FLOAT(3.2) NOT NULL,
 PRIMARY KEY  (id_lista)
 );
+
+INSERT INTO Voo (data_ida, data_volta, destino, origem, preco, ativo) 
+VALUES ('10/10/1010', '10/10/1111', 'são paulo', 'chile', 123.45, true);
+
+INSERT INTO Hotel (nome_hotel, data_entrada, data_saida, quantidade_quartos, quantidade_hospedes, preco, ativo) 
+VALUES ('five stars', '10/10/1010', '10/10/1011', 10, 1, 123.45, true);
+
+INSERT INTO Funcionario (cargo, departamento, filial, celular, telefone, email, numero, cep, 
+rua, bairro, cidade, complemento, nome, cpf, sexo, ativo, data_nasc, login, senha, acesso) 
+VALUES ('Vendedor', 'Vendas', 'SP', '(11)13213-4654', '(11)1323-4654', 'aohfa@aosfi.com', 654, '23132-312', 
+'rua tal de sp', 'jardim', 'SP', '-', 'Rafael', '012.123.123-12', 'M', true, '12/12/1212', 'admin', 'admin', 'MASTER');
+
+INSERT INTO Cliente (celular, telefone, email, numero, cep, rua, 
+bairro, cidade, complemento, nome, cpf, sexo, ativo, data_nasc) 
+VALUES ('(11)13213-4654', '(11)1323-4654', 'aohfa@aosfi.com', 654, '23132-312', 
+'rua tal de sp', 'jardim', 'SP', '-', 'Rafael', '012.123.123-12', 'M', true, '12/12/1212');
+
+INSERT INTO Venda (id_cliente, id_funcionario, ativo, total_preco, data_venda) 
+VALUES (1, 1, true, 100.10, '10/10/1010');
