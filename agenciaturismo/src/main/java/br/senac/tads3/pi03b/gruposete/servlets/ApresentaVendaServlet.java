7a6b5007a7b622e57d1f5cd397b756542389005c
ArtCouSan <artcousan@gmail.com>
2017-06-01 22:50:03 -0300

    Merge origin/master
    Conflicts:
    	agenciaturismo/src/main/java/br/senac/tads3/pi03b/gruposete/dao/VendaDAO.java
    	agenciaturismo/src/main/java/br/senac/tads3/pi03b/gruposete/resources/database.sql
    	agenciaturismo/src/main/java/br/senac/tads3/pi03b/gruposete/servlets/VendaServlet.java

---
+++ agenciaturismo/src/main/java/br/senac/tads3/pi03b/gruposete/servlets/ApresentaVendaServlet.java
@@ -0,0 +1,32 @@
+package br.senac.tads3.pi03b.gruposete.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+
+@WebServlet(name = "ApresentaVendaServlet", urlPatterns = {"/BuscarRelatorio"})
+public class ApresentaVendaServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/Relatorios/BuscarRelatorio.jsp");
+        dispatcher.forward(request, response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/Relatorios/BuscarRelatorio.jsp");
+        dispatcher.forward(request, response);
+
+    }
+
+}
