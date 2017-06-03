547f1000d5f7f685d05cb61ebe822aed648ed58e
ArtCouSan <artcousan@gmail.com>
2017-06-03 19:39:22 -0300

    Merge origin/master
    Conflicts:
    	agenciaturismo/src/main/java/br/senac/tads3/pi03b/gruposete/dao/VendaDAO.java
    	agenciaturismo/src/main/java/br/senac/tads3/pi03b/gruposete/resources/database.sql
    	agenciaturismo/src/main/java/br/senac/tads3/pi03b/gruposete/servlets/VendaServlet.java

--- agenciaturismo/src/main/webapp/bootstrap/js/relatoriosVenda.js
+++ agenciaturismo/src/main/webapp/bootstrap/js/relatoriosVenda.js
@@ -8,48 +8,48 @@ function pesquisar() {
 
 }
 
-function listar(evt) {
-
-    var texto = (evt.target.responseText);
-
-    var obj = JSON.parse(texto);
-
-    var table = document.getElementById("resultado");
-
-    while (table.firstChild) {
-        table.removeChild(table.firstChild);
-    }
-
-    for (var item in obj) {
-
-        var tr2 = document.createElement("tr");
-
-        var tdF = document.createElement("td");
-        var funcionarioD = document.createTextNode(obj[item].id_funcionario);
-        tdF.appendChild(funcionarioD);
-        var id1 = document.createAttribute("id");
-        id1.value = "funcionario" + obj[item].id_venda;
-        tdF.setAttributeNode(id1);
-
-        var tdC = document.createElement("td");
-        var clienteD = document.createTextNode(obj[item].id_cliente);
-        tdC.appendChild(clienteD);
-        var id2 = document.createAttribute("id");
-        id2.value = "cliente" + obj[item].id_venda;
-        tdC.setAttributeNode(id2);
-
-        var tdT = document.createElement("td");
-        var total_pD = document.createTextNode(obj[item].total_preco);
-        tdT.appendChild(total_pD);
-        var id3 = document.createAttribute("id");
-        id3.value = "total_preco" + obj[item].id_venda;
-        tdT.setAttributeNode(id3);
-
-        tr2.appendChild(tdF);
-        tr2.appendChild(tdC);
-        tr2.appendChild(tdT);
-    }
-
-    table.appendChild(tr2);
-
-}
+//function listar(evt) {
+//
+//    var texto = (evt.target.responseText);
+//
+//    var obj = JSON.parse(texto);
+//
+//    var table = document.getElementById("resultado");
+//
+//    while (table.firstChild) {
+//        table.removeChild(table.firstChild);
+//    }
+//
+//    for (var item in obj) {
+//
+//        var tr2 = document.createElement("tr");
+//
+//        var tdF = document.createElement("td");
+//        var funcionarioD = document.createTextNode(obj[item].id_funcionario);
+//        tdF.appendChild(funcionarioD);
+//        var id1 = document.createAttribute("id");
+//        id1.value = "funcionario" + obj[item].id_venda;
+//        tdF.setAttributeNode(id1);
+//
+//        var tdC = document.createElement("td");
+//        var clienteD = document.createTextNode(obj[item].id_cliente);
+//        tdC.appendChild(clienteD);
+//        var id2 = document.createAttribute("id");
+//        id2.value = "cliente" + obj[item].id_venda;
+//        tdC.setAttributeNode(id2);
+//
+//        var tdT = document.createElement("td");
+//        var total_pD = document.createTextNode(obj[item].total_preco);
+//        tdT.appendChild(total_pD);
+//        var id3 = document.createAttribute("id");
+//        id3.value = "total_preco" + obj[item].id_venda;
+//        tdT.setAttributeNode(id3);
+//
+//        tr2.appendChild(tdF);
+//        tr2.appendChild(tdC);
+//        tr2.appendChild(tdT);
+//    }
+//
+//    table.appendChild(tr2);
+//
+//}
